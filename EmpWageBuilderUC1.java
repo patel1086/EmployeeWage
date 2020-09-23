8fad96ab62bad13555a3e835e0c521f39aab4032
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-23 15:22:23 +0530

    [Jitendra]Added:Cal Employee Wages

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,10 +1,15 @@
 public class EmpWageBuilderUC1{
 	public static void main(String[] args){
 	int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+	int empHrs=0;
+        int empWage=0;
 	double empCheck=Math.floor(Math.random()*10)%2;
 	if(empCheck==IS_FULL_TIME)
-		System.out.println("Employee is Present");
+		empHrs=8;
 	else
-		System.out.println("Employee is ABsent");
+		empHrs=0;
+	empWage=empHrs*EMP_RATE_PER_HOUR;
+	System.out.println("Emp Wage: " +empWage);
 }
 }
