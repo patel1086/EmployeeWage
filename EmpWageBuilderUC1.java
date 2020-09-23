661febf26619e7cea1d9d06d13676ff3032808f8
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-23 15:57:35 +0530

    [Jitendra]Added: Total Wage of Month

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -2,18 +2,26 @@ public class EmpWageBuilderUC1{
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 	public static void main(String[] args){
 
 	int empHrs=0;
         int empWage=0;
-	double empCheck=Math.floor(Math.random()*10)%3;
-	if(empCheck==IS_FULL_TIME)
-		empHrs=4;
-	else if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-	else
-		empHrs=0;
+	int totalEmpWage=0;
+	for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+	{
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		switch(empCheck){
+			case IS_PART_TIME:
+				empHrs=4;
+			case IS_FULL_TIME:
+				empHrs=8;
+			default:
+				empHrs=0;
+			}
 	empWage=empHrs*EMP_RATE_PER_HOUR;
-	System.out.println("Emp Wage: " +empWage);
+	totalEmpWage+=empWage;
+	}
+	System.out.println("Total Emp Wage: " +totalEmpWage);
 }
 }
