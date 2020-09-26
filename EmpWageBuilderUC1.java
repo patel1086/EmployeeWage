6ff25d87024859937d7acbb70e0bb075264a74a0
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-26 23:30:25 +0530

    [Jitendra] Added: Wage using Interface approach

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,55 +1,105 @@
 public class EmpWageBuilderUC1{
-	public static final int IS_PART_TIME=1;
-	public static final int IS_FULL_TIME=2;
-	
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
 
-	public EmpWageBuilderUC1(String company,int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth){
-	this.company=company;
-	this.empRatePerHour=empRatePerHour;
-	this.numOfWorkingDays=numOfWorkingDays;
-	this.maxHoursPerMonth=maxHoursPerMonth;
-	}
+	public static class ComputWage{
 	
-	public void computeEmpWage(){
-	int empHrs=0;
-        int totalEmpHrs=0;
-	int totalWorkingDays=0;
-	
-	while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays)
+
+	public String company;
+	public int emprate;
+	public int wrkingdays;
+	public int wrkinghrs;
+	public int totalwage;
+
+	public ComputWage(String company,int emprate,int wrkingdays, int wrkinghrs)
 	{
-		totalWorkingDays++;
-		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		switch(empCheck){
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			default:
-				empHrs=0;
-			}
-	totalEmpHrs+=empHrs;
-	System.out.println("DAy#: " + totalWorkingDays + "Emp Hr: " +empHrs);
+		this.company=company;
+		this.emprate=emprate;
+		this.wrkingdays=wrkingdays;
+		this.wrkinghrs=wrkinghrs;
 	}
-	totalEmpWage=totalEmpHrs*empRatePerHour;
+	public void setTotalWage(int totalwage)
+	{
+		this.totalwage=totalwage;
+	}
+	public String toString()
+{
+	return "Total emp wage for company: "+company+" is "+totalwage;
+}
+}
+
+public static class EmpBuilder 
+{
+	static final int IS_FULL_TIME=2;
+	static final int IS_PART_TIME=1;
+	public ComputWage[] compwagearray;
+	private int n=0;
+
+	public EmpBuilder()
+	{
+		compwagearray= new ComputWage[5];
+
+	}
+	public void addEmployeeWage(String company,int emprate,int wrkingdays, int wrkinghrs)
+	{
+		compwagearray[n]=new ComputWage(company,emprate,wrkingdays,wrkinghrs);
+		n++;
+	}
+	public void computeCompanyWage()
+	{
+		for(int i=0;i<n;i++)
+		{
+			compwagearray[i].setTotalWage(ComputeWage(compwagearray[i]));
+			System.out.println(compwagearray[i]);
+		}
+	}
+	public int ComputeWage(ComputWage pil)
+{
+	int employeehrs=0;
+	int totalworkdays=0;
+	int totalemphrs=0;
 	
+	
+	while(totalemphrs<=pil.wrkinghrs&&totalworkdays<pil.wrkingdays)
+{
+	totalworkdays++;
+	int empcheck=(int) Math.floor(Math.random()*10)%3;
+
+	switch(empcheck)
+	{
+		case 1:
+		employeehrs=4;
+		break;
+		case 2:
+		employeehrs=8;
+		break;
+		default:
+		employeehrs=0;
+	}
+
+
+	
+	totalemphrs+=employeehrs;
+	System.out.println("Emp hrs for day "+totalworkdays+" is: "+employeehrs);
+
+
 }
-	@Override
-	public String toString(){
-	return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+	return totalemphrs* pil.emprate;
+
+
 }
-	public static void main(String[] args){
-	EmpWageBuilderUC1 Lafarge=new EmpWageBuilderUC1("Lafarge",20,2,10);
-	EmpWageBuilderUC1 Jsk=new EmpWageBuilderUC1("JsK",10,4,20);
-	Lafarge.computeEmpWage();
-	System.out.println(Lafarge);
-	Jsk.computeEmpWage();
-	System.out.println(Jsk);
+
+
 }
+
+
+	
+public static void main(String[] args) {
+	
+	
+	EmpBuilder esr= new EmpBuilder();
+	esr.addEmployeeWage("Lafarge",20,2,10);
+	esr.addEmployeeWage("Shri",10,4,20);
+	esr.computeCompanyWage();
+
+}
+
 }
